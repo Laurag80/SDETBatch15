72d55d5ca9deab0a550c3833d17f68aec40051f6
Laurag <lauragasparyan87@gmail.com>
2023-01-22 14:28:14 -0800

    updatedChanges

--- BreakKeywordDemo2.java
+++ BreakKeywordDemo2.java
@@ -1,19 +1,19 @@
 package com.syntax.class08;
 
-public class BreakDemo2 {
+public class BreakKeywordDemo2 {
     public static void main(String[] args) {
-
         // create below program without break
 
         int temp=75;
 
         while (temp<=105){
             if(temp<=100){
-            System.out.println("I love summer "+ temp);
-        }else{
+                System.out.println("I love summer "+ temp);
+            }else{
                 System.out.println("It's very hot "+temp);
             }
-          temp+=5;
+            temp+=5;
         }
     }
 }
+
